86065377d002433362532369c8066d1a312fa4a0
subzero <shubham8111994@gmail.com>
2023-03-15 18:57:36 +0530

    Initial commit

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,6 @@
+//Program to show best deals in hotel reservation.
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation system");
+    }
+}
